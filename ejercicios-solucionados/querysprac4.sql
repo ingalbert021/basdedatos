ae4c5029d59e5722d99fe43a1cfa9a727f288ada
ingalbert021 <albertocj54@gmail.com>
2024-04-14 18:25:51 -0400

    agregue soluciones del querysprac4

--- ejercicios-solucionados/querysprac4.sql
+++ ejercicios-solucionados/querysprac4.sql
@@ -3,8 +3,22 @@ use Northwind
 /*1 ejercicio*/
 select count(OrderID) as cantidad, min(Freight) as pesoMinimo,  max(Freight) as pesomaximo, avg(freight) as pesopromedio, sum(freight) as pesototal from  Orders 
 
-/*2ejercicio*/
-
-select count(title), lastname, firstname from Employees 
-where Title = Title
+/*2 ejercicio*/
+select title, count(Title) as cantidad  from Employees
 group by Title
+order by title desc
+
+/*3 ejercicio*/
+
+
+select categoryID, count(UnitsInStock) as CantidaddeProductos, avg(unitprice) as preciopromedio from Products
+group by CategoryID
+
+
+/*4 ejercicio*/
+select * from Orders
+select * from Employees
+select * from Products
+
+select datename(year, requireddate) as year, EmployeeID from Orders
+group by OrderID, RequiredDate, EmployeeID
